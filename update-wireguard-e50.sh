3dda2137a6d0a6fabc7cc0d1861cd4f9f7b105d3
Kolby <kolbyg@users.noreply.github.com>
2019-12-20 08:45:51 -0800

    Update and rename update-wireguard.sh to update-wireguard-e50.sh

--- update-wireguard-e50.sh
+++ update-wireguard-e50.sh
@@ -12,12 +12,12 @@ downloads=/config/data/install-packages
 scripts=/config/scripts
 
 cd ~
-curl https://raw.githubusercontent.com/BSPDevelopers/edgeos-resources/master/refresh-dns.sh --output refresh-dns.sh
+curl https://raw.githubusercontent.com/kolbyg/edgeos-resources/master/refresh-dns.sh --output refresh-dns.sh
 sudo install -o root -g root -m 0755 refresh-dns.sh /config/scripts/refresh-dns.sh
 
 sudo mkdir -p $downloads
 cd $downloads
-curl -LJ https://github.com/BSPDevelopers/edgeos-resources/raw/master/wireguard-v2.0-e50.deb --output wireguard.deb
+curl -LJ https://github.com/kolbyg/edgeos-resources/raw/master/wireguard-v2.0-e50.deb --output wireguard.deb
 
 for pkg in *; do
   dpkg-query -W --showformat='${Status}\n' $(dpkg --info "${pkg}" | grep "Package: " | awk -F' ' '{ print $NF}') > /dev/null 2>&1 || (dpkg -i ${pkg} && reboot now)
